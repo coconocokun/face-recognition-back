b64e6c216ec56b7de971981b1eb234134d42b412
GavinKwon <cocoforuiuc@gmail.com>
2024-04-18 16:19:58 +0900

    face recognition

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 import express from "express";
 import bodyParser from "body-parser";
+import multer from "multer";
+import "@tensorflow/tfjs-node";
+import { Canvas, Image, ImageData, loadImage } from "canvas";
+import * as faceapi from "face-api.js";
+
+//@ts-ignore
+faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
 
 const app = express();
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename(req, file, cb) {
+    cb(null, "face.jpg");
+  },
+});
+const upload = multer({ storage });
 
 app.use(bodyParser.json());
 
@@ -14,13 +31,25 @@ app.get("/hello", (req, res) => {
   });
 });
 
-app.post("/face-recognition", async (req, res) => {
+app.post("/face-recognition", upload.single("image"), async (req, res) => {
   // 1. Get the picture
+  const file = req.file;
+  if (!file) {
+    return res.status(400).send({
+      error: "No file",
+    });
+  }
   // 2. Call the faceapi function
+  const img = await loadImage("./uploads/face.jpg");
+  //@ts-ignore
+  const result = await faceapi.detectSingleFace(img);
   // 3. Best match
   // 4. Return the results
+  return res.status(200).send(result);
 });
 
-app.listen(3000, () => {
+app.listen(3000, async () => {
+  await faceapi.nets.ssdMobilenetv1.loadFromDisk("./models");
+  await faceapi.nets.faceRecognitionNet.loadFromDisk("./models");
   console.log("Server running on http://localhost:3000");
 });
